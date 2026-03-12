a5ff47a72853e3fc3b345d9194f31d319a54145c
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-03-12 23:07:42 +0100

    Add a flake with support for builsing docker images

---
+++ flake.nix
@@ -0,0 +1,98 @@
+{
+  description = "stromer2mqtt";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      self,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+
+        packages = {
+          # regular, host-native build (dynamic)
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "stromer2mqtt";
+            version = "1.0.0";
+
+            src = ./.;
+
+            # assuming you have a Cargo.lock
+            cargoLock.lockFile = ./Cargo.lock;
+          };
+        }
+        // (
+          # musl-static package, only on Linux
+          if pkgs.stdenv.isLinux then
+            {
+              static = pkgs.pkgsStatic.rustPlatform.buildRustPackage {
+                pname = "stromer2mqtt";
+                version = "1.0.0";
+
+                src = ./.;
+
+                cargoLock.lockFile = ./Cargo.lock;
+              };
+            }
+          else
+            { }
+        )
+        // {
+          # Add dockerImage to packages
+
+          dockerImage =
+            let
+              # Select the appropriate package (static for Linux, default otherwise)
+              stromer2mqttApp =
+                if pkgs.stdenv.isLinux then self.packages.${system}.static else self.packages.${system}.default;
+
+              # Create an FHS-like environment for the application.
+              # This ensures the binary is at a predictable path like /app/bin/stromer2mqtt
+              # along with its runtime dependencies.
+              appFhs = pkgs.buildEnv {
+                name = "stromer2mqtt-fhs";
+                paths = [
+                  stromer2mqttApp
+                ];
+                postBuild = ''
+                  mkdir -p $out/app/bin
+                  ln -s ${stromer2mqttApp}/bin/stromer2mqtt $out/app/bin/stromer2mqtt
+                '';
+              };
+
+              app = pkgs.buildEnv {
+                name = "stromer2mqtt";
+                paths = [
+                  appFhs
+                ];
+              };
+            in
+            pkgs.dockerTools.buildImage {
+              name = "stromer2mqtt";
+              tag = "latest";
+              copyToRoot = app;
+
+              config = {
+                Cmd = [ "/app/bin/stromer2mqtt" ]; # The command to run when the container starts
+              };
+            };
+        };
+      }
+    );
+}
